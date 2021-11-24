137ede4145ddbd6e9d297e9f29ed89df3a269cab
BaoXuebin <baoxbin@hotmail.com>
2021-11-24 17:59:16 +0800

    fix icon path

--- src/components/AccountIcon.js
+++ src/components/AccountIcon.js
@@ -1,5 +1,5 @@
 import React from 'react'
 
-const AccountIcon = ({ iconType, ...rest }) => <img {...rest} src={`../../icons/${iconType}.png`} alt="" width={32} height={32} />
+const AccountIcon = ({ iconType, ...rest }) => <img {...rest} src={`../../web/icons/${iconType}.png`} alt="" width={32} height={32} />
 
 export default AccountIcon
